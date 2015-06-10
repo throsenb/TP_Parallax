3a63b6075efba1c3a8a5578fa8740242ccb35b80
throsenb <shelljunzhu@Mark-Rosenbergs-iMac-2.local>
2015-06-10 16:35:58 -0400

    Hover Text Animation
    Text changes on modal hover state.
    Logo closes on mouseLeave state

--- js/app.js
+++ js/app.js
@@ -10,18 +10,44 @@
 
 
 	// // Version 2
+    // var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
+    //     $text = $( '#homeHero h1:nth-of-type(2)' ),
+    //     delay = 4; //seconds
+    
+    // function loop ( delay ) {
+    //     $.each( items, function ( i, elm ){
+    //         $text.delay( delay*1E3).fadeOut();
+    //         $text.queue(function(){
+    //             $text.html( items[i] );
+    //             $text.dequeue();
+    //         });
+    //         $text.fadeIn(10);
+    //         $text.queue(function(){
+    //             if ( i == items.length -1 ) {
+    //                 loop(delay);   
+    //             }
+    //             $text.dequeue();
+    //         });
+    //     });
+    // }
+
+    // loop( delay );
+
+
+	// // Version 3
  //    var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
  //        $text = $( '#homeHero h1:nth-of-type(2)' ),
- //        delay = 4; //seconds
+ //        delay = 0; //seconds
     
  //    function loop ( delay ) {
  //        $.each( items, function ( i, elm ){
- //            $text.delay( delay*1E3).fadeOut();
+ //            $text.delay(4000).fadeOut(0);
  //            $text.queue(function(){
  //                $text.html( items[i] );
  //                $text.dequeue();
+ //                $('#logoOrange').toggleClass('open');
  //            });
- //            $text.fadeIn(10);
+ //            $text.fadeIn(0);
  //            $text.queue(function(){
  //                if ( i == items.length -1 ) {
  //                    loop(delay);   
@@ -31,29 +57,50 @@
  //        });
  //    }
 
- //    loop( delay );
 
 
-	// Version 3
-    var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
-        $text = $( '#homeHero h1:nth-of-type(2)' ),
-        delay = 0; //seconds
-    
-    function loop ( delay ) {
-        $.each( items, function ( i, elm ){
-            $text.delay(4000).fadeOut(0);
-            $text.queue(function(){
-                $text.html( items[i] );
-                $text.dequeue();
-            });
-            $text.fadeIn(0);
-            $text.queue(function(){
-                if ( i == items.length -1 ) {
-                    loop(delay);   
-                }
-                $text.dequeue();
-            });
-        });
-    }
+
+// Version 4
+
+
+
+ 	$('#modal').mouseenter(function() {
+		$('#logoOrange').animate({marginLeft: "75%"});
+
+
+	    var items = ["Is Moving Forward"],
+	        $text = $( '#homeHero h1:nth-of-type(2)' ),
+	        delay = 0; //seconds
+	    
+	    function loop ( delay ) {
+	        $.each( items, function ( i, elm ){
+	            $text.delay( delay*1E3).fadeOut();
+	            $text.queue(function(){
+	                $text.html( items[i] );
+	                $text.dequeue();
+	            });
+	            $text.fadeIn(10);
+	            $text.queue(function(){
+	                if ( i == items.length -1 ) {
+	                    // loop(delay);   
+	                }
+	                $text.dequeue();
+	            });
+	        });
+	    }
+
+	    loop( delay );
+
+
+ 		console.log('mouseEnter');
+	});
+
+ 	$('#modal').mouseleave(function() {
+ 		$('#logoOrange').animate({marginLeft: "0%"});
+ 		console.log('mouseLeave');
+	});
+
+
+
 
     loop( delay );

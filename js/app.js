8cc4a88410d8b7288cd31ec3ad700fa7efa97a61
throsenb <shelljunzhu@Mark-Rosenbergs-iMac-2.local>
2015-06-10 15:38:48 -0400

    Looped Text Animation
    Cycle of text html changes
    Still need to be synced up to moving logo timeline.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
+// Version 1
+    // $("#homeHero h1:nth-of-type(1)").delay(100).fadeOut(500, function() {
+    //     $(this).text('Are We').fadeIn(500);
+    // });
 
-    $("#homeHero h1:nth-of-type(1)").delay(100).fadeOut(500, function() {
-        $(this).text('Are We').fadeIn(500);
-    });
+    // $("#homeHero h1:nth-of-type(2)").delay(100).fadeOut(500, function() {
+    //     $(this).text('Parallel with you?').fadeIn(500);
+    // });
 
-    $("#homeHero h1:nth-of-type(2)").delay(100).fadeOut(500, function() {
-        $(this).text('Parallel with you?').fadeIn(500);
-    });
 
+
+
+    var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
+        $text = $( '#homeHero h1:nth-of-type(2)' ),
+        delay = 4; //seconds
+    
+    function loop ( delay ) {
+        $.each( items, function ( i, elm ){
+            $text.delay( delay*1E3).fadeOut();
+            $text.queue(function(){
+                $text.html( items[i] );
+                $text.dequeue();
+            });
+            $text.fadeIn();
+            $text.queue(function(){
+                if ( i == items.length -1 ) {
+                    loop(delay);   
+                }
+                $text.dequeue();
+            });
+        });
+    }
+
+    loop( delay );

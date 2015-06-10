7a209dfa6599c5349e46eae809d3d733245149e7
throsenb <shelljunzhu@Mark-Rosenbergs-iMac-2.local>
2015-06-10 15:32:04 -0400

    Animated Text
    Added basic single loop animated text with jQuery.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+    $("#homeHero h1:nth-of-type(1)").delay(100).fadeOut(500, function() {
+        $(this).text('Are We').fadeIn(500);
+    });
+
+    $("#homeHero h1:nth-of-type(2)").delay(100).fadeOut(500, function() {
+        $(this).text('Parallel with you?').fadeIn(500);
+    });
+

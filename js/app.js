c4a9fb934e141c7ca093288057b8b81bac530ab4
throsenb <shelljunzhu@Mark-Rosenbergs-iMac-2.local>
2015-06-10 17:37:06 -0400

    Cycle on Hover state
    One cycle per hover state.

--- js/app.js
+++ js/app.js
@@ -63,33 +63,77 @@
 // Version 4
 
 
+//DOM ELEMENTS
+// $("div").each(function(index, value) { 
+//     console.log('div' + index + ':' + $(this).attr('id')); 
+// });
+// //outputs the ids of every div on the web page
+// //ie - div1:header, div2:body, div3:footer
+ 
+// //ARRAYS
+// var arr = [ "one", "two", "three", "four", "five" ];
+// jQuery.each(arr, function(index, value) {
+//        console.log(this);
+//        return (this != "three"); // will stop running after "three"
+//    });
+// //outputs: one two three
+ 
+//OBJECTS
+var obj = { one:1, two:2, three:3, four:4, five:5 };
+    $.each(obj, function(i, val) {
+       console.log(val);
+    });
+//outputs: 1 2 3 4 5
+
+		var arr = ["Is Moving Forward", "Begins Today", "Is With Your Future"];
+		var index = 0;
 
  	$('#modal').mouseenter(function() {
 		$('#logoOrange').animate({marginLeft: "75%"});
 
+	// // Method 1
+		// var nextWord = (function() {
+		// 	var wordArray = ['fe','fi','fo','fum'];
+		// 	var count = -1;
+		// 	return function() {
+		// 		return wordArray[++count % wordArray.length];
+		//   	}
+		// }());
 
-	    var items = ["Is Moving Forward"],
-	        $text = $( '#homeHero h1:nth-of-type(2)' ),
-	        delay = 0; //seconds
+		// $('#homeHero h1:nth-of-type(2)').innerHTML = nextWord();
+
+
+	// // Method 2
+	    // var items = ["Is Moving Forward", "Likes Puppies"],
+	    //     $text = $( '#homeHero h1:nth-of-type(2)' ),
+	    //     delay = 0; //seconds
 	    
-	    function loop ( delay ) {
-	        $.each( items, function ( i, elm ){
-	            $text.delay( delay*1E3).fadeOut();
-	            $text.queue(function(){
-	                $text.html( items[i] );
-	                $text.dequeue();
-	            });
-	            $text.fadeIn(10);
-	            $text.queue(function(){
-	                if ( i == items.length -1 ) {
-	                    // loop(delay);   
-	                }
-	                $text.dequeue();
-	            });
-	        });
-	    }
+	    // function loop ( delay ) {
+	    //     $.each( items, function ( i, elm ){
+	    //         $text.delay( delay*1E3).fadeOut();
+	    //         $text.queue(function(){
+	    //             $text.html( items[i] );
+	    //             $text.dequeue();
+	    //         });
+	    //         $text.fadeIn(10);
+	    //         $text.queue(function(){
+	    //             if ( i == items.length -1 ) {
+	    //                 // loop(delay);   
+	    //             }
+	    //             $text.dequeue();
+	    //         });
 
-	    loop( delay );
+	    //     });
+	    // }
+
+	    // loop( delay );
+
+
+
+
+		$('#homeHero h1:nth-of-type(2)').html(arr[index]); 
+		index = (index + 1) % arr.length ;
+
 
 
  		console.log('mouseEnter');
@@ -97,6 +141,7 @@
 
  	$('#modal').mouseleave(function() {
  		$('#logoOrange').animate({marginLeft: "0%"});
+ 			    
  		console.log('mouseLeave');
 	});
 

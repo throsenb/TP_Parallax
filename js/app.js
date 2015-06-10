c4ffe35d148c6d64b9c108db9fe82d76701dfb5e
throsenb <shelljunzhu@Mark-Rosenbergs-iMac-2.local>
2015-06-10 15:56:57 -0400

    Synced Text Animation
    -Updated Text animation that syncs with logo movement.

--- js/app.js
+++ js/app.js
@@ -9,19 +9,44 @@
 
 
 
+	// // Version 2
+ //    var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
+ //        $text = $( '#homeHero h1:nth-of-type(2)' ),
+ //        delay = 4; //seconds
+    
+ //    function loop ( delay ) {
+ //        $.each( items, function ( i, elm ){
+ //            $text.delay( delay*1E3).fadeOut();
+ //            $text.queue(function(){
+ //                $text.html( items[i] );
+ //                $text.dequeue();
+ //            });
+ //            $text.fadeIn(10);
+ //            $text.queue(function(){
+ //                if ( i == items.length -1 ) {
+ //                    loop(delay);   
+ //                }
+ //                $text.dequeue();
+ //            });
+ //        });
+ //    }
 
+ //    loop( delay );
+
+
+	// Version 3
     var items = ["Is Moving Forward", "Begins Today", "Is With Your Future"],
         $text = $( '#homeHero h1:nth-of-type(2)' ),
-        delay = 4; //seconds
+        delay = 0; //seconds
     
     function loop ( delay ) {
         $.each( items, function ( i, elm ){
-            $text.delay( delay*1E3).fadeOut();
+            $text.delay(4000).fadeOut(0);
             $text.queue(function(){
                 $text.html( items[i] );
                 $text.dequeue();
             });
-            $text.fadeIn();
+            $text.fadeIn(0);
             $text.queue(function(){
                 if ( i == items.length -1 ) {
                     loop(delay);   
